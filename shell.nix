ba8897922c858e47c64b959138fadc8c70c75c81
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-10-05 10:17:22 +0200

    setup nix env

---
+++ shell.nix
@@ -0,0 +1,14 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+    name = "test-editor-xtext-gradle";
+    buildInputs = [
+        jdk8
+    ];
+    shellHook = ''
+        # do some gradle "finetuning"
+        alias g="./gradlew"
+        alias g.="../gradlew"
+        export GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.daemon=false"
+    '';
+}
