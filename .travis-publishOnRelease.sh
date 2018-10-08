d3dd96953045c76930ed45c3ec5aff62e515d1d4
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-10-08 16:18:37 +0200

    enable publishing

--- .travis-publishOnRelease.sh
+++ .travis-publishOnRelease.sh
@@ -4,9 +4,5 @@
 if [[ -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
     version="${TRAVIS_TAG//v}"
     echo "Publishing version: $version"
-
-    # Deploy Maven artifacts
-    cp .travis.settings.xml $HOME/.m2/settings.xml
-    ./gradlew deploy
-
+    ./gradlew bintrayUpload
 fi
