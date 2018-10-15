c3fafec254d246bf1f59695cb3716a2ba2049ee5
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-10-15 14:15:34 +0200

    build with java 10

--- shell.nix
+++ shell.nix
@@ -3,13 +3,15 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "test-editor-xtext-gradle";
     buildInputs = [
-        jdk8
+        jdk10
         travis
     ];
     shellHook = ''
         # do some gradle "finetuning"
         alias g="./gradlew"
         alias g.="../gradlew"
-        export GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.daemon=false"
+        export GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.daemon=false -Dfile.encoding=utf-8"
+        # in case of any local java installations
+        export JAVA_HOME=$(readlink $(dirname $(which java)))/..
     '';
 }
